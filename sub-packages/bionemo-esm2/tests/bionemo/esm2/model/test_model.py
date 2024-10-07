ddd2001a188b50709a3a9f835ece26bb9c505492
John St John <jstjohn@nvidia.com>
2024-10-07 20:15:09 +0000

    Revert "Add a test that catches this checkpoint issue in main"
    This reverts commit 5ee6641d9fc3ee205052c2ee99d2ee930d69af57.

--- sub-packages/bionemo-esm2/tests/bionemo/esm2/model/test_model.py
+++ sub-packages/bionemo-esm2/tests/bionemo/esm2/model/test_model.py
@@ -23,9 +23,7 @@ from unittest import mock
 
 import pytest
 import torch
-from megatron.core.transformer.module import Float16Module
 from nemo.collections.common.tokenizers.huggingface.auto_tokenizer import AutoTokenizer
-from nemo.lightning import io as nio
 from torch import Tensor
 from transformers import EsmForMaskedLM
 
@@ -42,8 +40,6 @@ from bionemo.testing.data.load import load
 
 
 nemo1_checkpoint_path: Path = load("esm2/nv_650m:1.0")
-nemo2_checkpoint_path: Path = load("esm2/650m:2.0")
-HF_MEAN_LOSS: float = 2.9279041290283203
 
 
 def reduce_hiddens(hiddens: Tensor, attention_mask: Tensor) -> Tensor:
@@ -236,7 +232,7 @@ def test_esm2_loss(esm2_650M_config_w_ckpt, dummy_protein_dataset, dummy_parquet
         random_numpy_context(seed),
     ):
         tokenizer = get_tokenizer()
-        esm2_650M_config_w_ckpt.set_hparam("precision")
+
         # ESM2 model initialized with 650M params
         model = esm2_650M_config_w_ckpt.configure_model(tokenizer).cuda()
 
@@ -277,65 +273,6 @@ def test_esm2_loss(esm2_650M_config_w_ckpt, dummy_protein_dataset, dummy_parquet
             hf_mean_loss = _compute_loss(hf_model, train_dataloader)
             print(f"hf_mean_loss: {hf_mean_loss}")
         else:
-            hf_mean_loss = torch.tensor(HF_MEAN_LOSS).cuda()
+            hf_mean_loss = torch.tensor(2.9279041290283203).cuda()
 
         torch.testing.assert_close(mean_loss, hf_mean_loss, atol=1e-4, rtol=0.0)
-
-
-def test_esm2_loss_nemo2_ckpt(esm2_650M_config_w_ckpt, dummy_protein_dataset, dummy_parquet_train_val_inputs):
-    train_cluster_path, valid_cluster_path = dummy_parquet_train_val_inputs
-    esm2_650M_config_w_ckpt = nio.reinit(esm2_650M_config_w_ckpt)
-    esm2_650M_config_w_ckpt.set_hparam("nemo1_ckpt_path", None)
-    esm2_650M_config_w_ckpt.set_hparam("initial_ckpt_path", nemo2_checkpoint_path)
-    compute_hf_reference: bool = False
-    seed: int = 42
-
-    with (
-        torch.inference_mode(),
-        megatron_parallel_state_utils.distributed_model_parallel_state(seed),
-        random_numpy_context(seed),
-    ):
-        tokenizer = get_tokenizer()
-        # ESM2 model initialized with 650M params
-        model = Float16Module(esm2_650M_config_w_ckpt, esm2_650M_config_w_ckpt.configure_model(tokenizer).cuda())
-
-        # Initialize the data module.
-        data_module = ESMDataModule(
-            train_cluster_path=train_cluster_path,
-            train_database_path=dummy_protein_dataset,
-            valid_cluster_path=valid_cluster_path,
-            valid_database_path=dummy_protein_dataset,
-            global_batch_size=4,
-            micro_batch_size=2,
-            min_seq_length=None,
-            max_seq_length=1024,
-            seed=seed,
-        )
-        assert data_module is not None
-        data_module.trainer = mock.Mock()
-        data_module.trainer.max_epochs = 1
-        data_module.trainer.max_steps = 10
-        data_module.trainer.val_check_interval = 2
-        data_module.trainer.limit_val_batches = 1
-
-        data_module.setup()
-
-        train_dataloader = data_module.train_dataloader()
-        assert isinstance(train_dataloader, torch.utils.data.DataLoader)
-
-        val_dataloader = data_module.val_dataloader()
-        assert isinstance(val_dataloader, torch.utils.data.DataLoader)
-
-        mean_loss = _compute_loss(model, train_dataloader, vocab_size=tokenizer.vocab_size)
-
-        if compute_hf_reference:
-            # HF model initialized with 650M params
-            hf_model = EsmForMaskedLM.from_pretrained(
-                "facebook/esm2_t33_650M_UR50D", torch_dtype=get_autocast_dtype(32)
-            ).cuda()
-            hf_mean_loss = _compute_loss(hf_model, train_dataloader)
-            print(f"hf_mean_loss: {hf_mean_loss}")
-        else:
-            hf_mean_loss = torch.tensor(HF_MEAN_LOSS).cuda()
-
-        torch.testing.assert_close(mean_loss, hf_mean_loss, atol=2e-3, rtol=0.0)
