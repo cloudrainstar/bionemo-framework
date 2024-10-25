6f5777548afd8cef8ceb5ddd70e47dbd8d234136
Savitha Srinivasan <savithas@savithas-workstation.nvidia.com>
2024-10-25 20:00:25 +0000

    Remove debugging changes

--- sub-packages/bionemo-geneformer/tests/bionemo/geneformer/test_model.py
+++ sub-packages/bionemo-geneformer/tests/bionemo/geneformer/test_model.py
@@ -15,7 +15,6 @@
 
 import functools
 import tarfile
-import tempfile
 from copy import deepcopy
 from pathlib import Path
 from typing import List, Tuple
@@ -126,11 +125,6 @@ TARGET_MEAN_LOSS: float = 2.368649959564209
 @pytest.fixture()
 def cells() -> List[List[str]]:
     return deepcopy(CELLS_FOR_TEST)
-
-
-@pytest.fixture(autouse=True)
-def clear_cuda_cache():
-    torch.cuda.empty_cache()
 
 
 @pytest.fixture
@@ -844,7 +838,6 @@ def _train_model_get_ckpt(
         micro_batch_size=batch_size,
         global_batch_size=batch_size,
         bypass_tokenizer_vocab=True,
-        num_workers=1,
     )
 
     checkpoint_callback = nl_callbacks.ModelCheckpoint(
@@ -915,7 +908,7 @@ def _train_model_get_ckpt(
 
 @pytest.mark.needs_gpu
 def test_continue_from_checkpoint_geneformer(
-    tmpdir, geneformer_config: GeneformerConfig, n_layers_test: int = 3, n_steps_train: int = 50, batch_size: int = 1
+    tmpdir, geneformer_config: GeneformerConfig, n_layers_test: int = 3, n_steps_train: int = 50, batch_size: int = 16
 ):
     base_geneformer_config = io.reinit(geneformer_config)  # generate a new copy by calling the cached init.
 
@@ -968,7 +961,7 @@ def test_continue_from_checkpoint_geneformer(
 
 @pytest.mark.needs_gpu
 def test_finetune_geneformer(
-    tmpdir, geneformer_config: GeneformerConfig, n_layers_test: int = 3, n_steps_train: int = 50, batch_size: int = 1
+    tmpdir, geneformer_config: GeneformerConfig, n_layers_test: int = 3, n_steps_train: int = 50, batch_size: int = 16
 ):
     base_geneformer_config = io.reinit(geneformer_config)  # generate a new copy by calling the cached init.
 
@@ -1078,48 +1071,3 @@ def test_finetune_geneformer_with_peft(
         assert all(not p.requires_grad for name, p in model.encoder.named_parameters() if "adapter" not in name)
         assert all(p.requires_grad for name, p in model.encoder.named_parameters() if "adapter" in name)
         assert all(p.requires_grad for p in model.regression_head.parameters())
-
-
-if __name__ == "__main__":
-    with tempfile.TemporaryDirectory() as tempdir:
-        autocast_dtype = get_autocast_dtype(MODEL_PRECISION)
-        config = GeneformerConfig(
-            model_cls=GeneformerModel,
-            num_layers=6,
-            hidden_size=256,
-            ffn_hidden_size=512,
-            num_attention_heads=4,
-            seq_length=2048,
-            fp16=autocast_dtype == torch.float16,  # normally handled by ptl precision plugin
-            bf16=autocast_dtype == torch.bfloat16,  # normally handled by ptl precision plugin
-            fp32_residual_connection=False,  # TODO(@jstjohn) check this
-            hidden_dropout=0.02,
-            init_method_std=0.02,
-            kv_channels=None,
-            apply_query_key_layer_scaling=False,
-            make_vocab_size_divisible_by=128,
-            masked_softmax_fusion=True,  # TODO(@jstjohn) check this
-            fp16_lm_cross_entropy=False,
-            params_dtype=autocast_dtype,
-            pipeline_dtype=autocast_dtype,
-            autocast_dtype=autocast_dtype,  # setting this speeds things up a lot
-            gradient_accumulation_fusion=False,  # THIS BREAKS STUFF, leave False
-            layernorm_zero_centered_gamma=False,  # TODO(@jstjohn) check this
-            layernorm_epsilon=1.0e-12,
-            activation_func=F.gelu,  # TODO(@jstjohn) check this
-            qk_layernorm=False,  # TODO(@jstjohn) check this
-            apply_residual_connection_post_layernorm=False,  # False is new default, True was BERT pub.
-            bias_activation_fusion=True,  # TODO(@jstjohn) check this
-            bias_dropout_fusion=True,  # TODO(@jstjohn) check this
-            get_attention_mask_from_fusion=True,
-            attention_dropout=0.1,  # historically ignored in nemo1, always set to 0.1
-            share_embeddings_and_output_weights=True,
-            enable_autocast=False,  # This has to be set to True if we use the mixed precision plugin
-            biobert_spec_option=BiobertSpecOption.bert_layer_with_transformer_engine_spec,
-            nemo1_ckpt_path=str(nemo1_checkpoint_path),
-            return_only_hidden_states=True,  # This is what we did in nemo1 for inference
-        )
-        # test_finetune_geneformer(Path(tempdir), config, n_layers_test= 3, n_steps_train=50, batch_size=1)
-        test_continue_from_checkpoint_geneformer(
-            Path(tempdir), config, n_layers_test=3, n_steps_train=50, batch_size=1
-        )
